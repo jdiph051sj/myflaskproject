39c64a06fbb2222e36454fbe49b7e8978d120ce4
jdiph051sj <jdiph051@gmail.com>
2021-11-22 09:32:36 +0200

    initDepAgain6

--- application.py
+++ application.py
@@ -1,6 +1,13 @@
-from flask import Flask
+#Create a Flask/Python wep app
+from flask import Flask 
+
+#Create an instance of FLask
 application = Flask(__name__)
 
+#Create an end-point for the app
 @application.route('/')
+
+#Define a function
 def hello_world():
+    #Fucntion that returns a string
     return 'Hello, Flask 1..2..>>3 DeployAgain1-2234'
